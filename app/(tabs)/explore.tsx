198460df7eb88d0eef5d91c4ef83396419370ce8
User <giddtkipscheruyot@gmail.com>
2025-07-13 19:35:14 +0300

    Drawer module added

--- app/(tabs)/explore.tsx
+++ app/(tabs)/explore.tsx
@@ -1,1 +1,14 @@
 import React from "react";
+import { Drawer } from 'expo-router/drawer';
+import { GestureHandlerRootView } from 'react-native-gesture-handler';
+
+export default function DrawerLayout() {
+    return(
+        <GestureHandlerRootView>
+            <Drawer>
+                <Drawer.Screen name="index" />
+                <Drawer.Screen name="explore" />
+            </Drawer>
+        </GestureHandlerRootView>
+    )
+}
