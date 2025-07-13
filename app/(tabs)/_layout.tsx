bf2241a84ff0bc9c4fb4e520dd6fc4d63057b27a
User <giddtkipscheruyot@gmail.com>
2025-07-13 18:23:52 +0300

    Will implement drawer navigation

--- app/(tabs)/_layout.tsx
+++ app/(tabs)/_layout.tsx
@@ -1,7 +1,7 @@
 import { Tabs } from 'expo-router';
 import React from 'react';
 import { Platform } from 'react-native';
-
+import { Drawer } from 'expo-router/drawer';
 import { HapticTab } from '@/components/HapticTab';
 import { IconSymbol } from '@/components/ui/IconSymbol';
 import TabBarBackground from '@/components/ui/TabBarBackground';
