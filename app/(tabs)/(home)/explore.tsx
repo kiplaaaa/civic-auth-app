940fced4da1afc92a3b50e3cf6b1833e43adfe1a
User <giddtkipscheruyot@gmail.com>
2025-07-26 13:10:01 +0300

    Enhance app.json to configure expo-secure-store with Android backup and Face ID permission; refactor Explore component to include FlatList for data rendering

--- app/(tabs)/(home)/explore.tsx
+++ app/(tabs)/(home)/explore.tsx
@@ -1,7 +1,38 @@
 import { Link } from 'expo-router';
+import React from 'react';
+import { StyleSheet, View, Text } from 'react-native';
+import { FlatList } from 'react-native-gesture-handler';
 
 export default function Explore() {
+
+    const Data= [
+        {id: 1, title: 'banana'},
+        {id: 2, title: 'banana'},
+        {id: 3, title: 'banana'},
+        {id: 4, title: 'banana'},
+    ]
+
+    const MyList = ({name, index}) =>(
+        <View>
+            <Text>{index}.{name}</Text>
+        </View>
+    )
     return (
-        <Link href='/(tabs)/(home)/feed'>Feed</Link>
+        <View style={styles.container}>
+            <Link href='/(tabs)/(home)/feed'>Feed</Link>
+            <FlatList 
+                data={Data} 
+                renderItem={({item})=><MyList index= {item.id} name={item.title}/>} 
+                keyExtractor={name => name.id.toString()}
+            />
+        </View>
     )
 }
+const styles= StyleSheet.create({
+    container:{
+        flex: 1,
+        backgroundColor: 'white',
+        justifyContent: 'center',
+        alignItems: 'center',
+    }
+})
