c180aa649fa7b345ff552cb9a0bfd48208f8a187
User <giddtkipscheruyot@gmail.com>
2025-08-07 22:44:44 +0300

    Fix import statement and refactor user state management in zustand context for improved structure

--- context/zustand.tsx
+++ context/zustand.tsx
@@ -1,46 +1,48 @@
-import {} getItem, setItem, deleteItemAsync } from 'expo-secure-store';
+import { getItem, setItem, deleteItemAsync } from 'expo-secure-store';
 import React from "react";
 import { create } from 'zustand';
-import { persist } from 'zustand/middleware';
+import { createJSONStorage, persist } from "zustand/middleware";
+
 type User = {
     user: string;
 } | null;
 
 type UserState = {
+    user: User,
     loading: boolean;
     login: ()=>void;
     logOut: ()=>void;
 };
 
-export const userStoreData = create
-    (persist<UserState>((set)=>{
-        loading: false,
-        login: ()=>{
-            set((state)=>{
-                return{
-                    ...state, 
-                    loading: true,
-                }
-            })
-        }
-         logOut: ()=>{
-            set((state)=>{
-                return{
-                    ...state,
-                    loading: false,
-                }
-            })
-        }        
-    }),
+export const userStoredData = create(
+    persist<UserState>(
+        (set)=>{
+            user: User,
+            loading: false,
+            login: ()=>{
+                set((state)=>{
+                    return{
+                        ...state,
+                        loading: true,
+                    }
+                });
+            },
+            logOut: ()=>{
+                set((state)=>{
+                    return{
+                        ...state,
+                        loading: false,
+                    }
+                })
+            },
+       }
+    ),
     {
-        name: 'storage Key',
-        storage: {()=> {
-            return {
-                getItem,
-                setItem,
-                removeItem: deleteItemAsync,
-                }
-            };
-        }}
-    }
+        name: 'Authorize',
+        storage: createJSONStorage(=>({
+            getItem,
+            setItem, 
+            deleteItemAsync,
+        }))
+    }      
 )
