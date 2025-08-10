11960124f93e23671c7ce30fefdcd549720f744e
User <giddtkipscheruyot@gmail.com>
2025-08-10 14:58:19 +0300

    Add SquareGoals component for displaying goal squares with press functionality

---
+++ components/squareBingoBoard.tsx
@@ -0,0 +1,29 @@
+import React from "react";
+import { Text, TouchableOpacity, StyleSheet} from "react-native";
+
+type SquareGoalsProps = {
+    goal: string;
+    onPress: () => void;
+}
+
+export default function SquareGoals({goal, onPress}: SquareGoalsProps) {
+    return (
+        <TouchableOpacity onPress={onPress} style={styles.square}>
+            <Text style={styles.text}>{goal}</Text>
+        </TouchableOpacity>
+    );
+   
+}
+const styles = StyleSheet.create({
+    square:{
+        height: 80,
+        width: 80,
+        backgroundColor: '#f0f0f0',
+        borderWidth: 1,
+        borderColor: '#ccc',        
+    },
+    text:{
+        fontSize: 16,
+        fontWeight: 'bold',
+    }
+})
