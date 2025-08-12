a8ba4d55f4905a685ce6c23a6e1e3db27891e261
User <giddtkipscheruyot@gmail.com>
2025-08-12 12:39:45 +0300

    Refactor SquaresBingoBoard to initialize goals dynamically and improve title handling in modal

--- app/(tabs)/squaresBingoBoard.tsx
+++ app/(tabs)/squaresBingoBoard.tsx
@@ -2,16 +2,22 @@ import SquareGoals from "@/components/squareBingoBoard";
 import React from "react";
 import { Modal, View, StyleSheet, SafeAreaView, Button, TextInput } from "react-native";
 export default function SquaresBingoBoard() {
-    const [ title, setTitle ] = React.useState("Add New Goals");
+    const [ title, setTitle ] = React.useState(
+        Array.from({length: 25}, (_, index)=> `Add Goal ${index + 1}`)
+    );
     const [ modalVisible, setModalVisible ] = React.useState(false);
+    const [ selectedIndex, setSelectedIndex ] = React.useState(null);
     const [ newTitle, setNewTitle ] = React.useState("");
 
-    const handleTitleChange = () => {
-        setNewTitle(title);        
+    const handleTitleChange = (index: any) => {
+        setSelectedIndex(index);
+        setNewTitle(title[index]);        
         setModalVisible(true);
     }
     const handleConfirm = () => {  
-        setTitle(newTitle);
+        setTitle((prevTitles) => prevTitles.map((goal, i) =>
+            (i === selectedIndex ? newTitle : goal ))
+        );
         setModalVisible(false);        
     }
 
@@ -19,13 +25,13 @@ export default function SquaresBingoBoard() {
         <SafeAreaView>
             <View style={styles.container}>
                 {
-                    Array.from({length: 25}, (_, i) => (
-                        <SquareGoals 
-                            key={i} 
-                            goal={title + ` ${i + 1}`} 
-                            onPress={handleTitleChange}                             
-                        />
-                    ))
+                    title.map((goal, index)=> (
+                        <SquareGoals
+                            key={index}
+                            goal={goal}
+                            onPress={()=> handleTitleChange(index)}                        
+                        />)
+                    )
                 }
                 <Modal
                     animationType="fade"
